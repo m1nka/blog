b52d745d823614af62ed31f64e7024abe412e6c7
Maximilian Jerg <spirit@MaximiliansMini.fritz.box>
2020-08-06 14:21:35 +0200

    major improvements

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
+// dark theme toggle
+function toggleTheme() {
+  if (!getTheme) {
+    setTheme('light');
+  }
+  if (getTheme() === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'light');
+    setTheme('light');
+    console.log('Set theme: light');
+  } else if (getTheme() === 'light') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    setTheme('dark');
+    console.log('Set theme: dark');
+  }
+}
+
+function getTheme() {
+  return window.localStorage.getItem('theme');
+}
+function setTheme(t) {
+  window.localStorage.setItem('theme', t);
+}
+
+
+// nav bar toggle
 let navbar = false;
 function toggleNav() {
   if (!navbar) {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("menu-toggle").classList.add('turnit');
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("main").classList.add('menu-open');
     navbar = true;
   } else {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("menu-toggle").classList.remove('turnit');
-    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("main").classList.remove('menu-open');
     navbar = false;
   }
-}
-let theme = false;
-function toggleTheme() {
-
 }
