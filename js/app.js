49f592e09e3d940d915de44e4c7998afc3623eb8
Maximilian Jerg <spirit@MaximiliansMini.fritz.box>
2020-09-25 16:30:22 +0200

    added animated particles and typer

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// particle.js
+
+particlesJS.load('particles-js', '/js/particles.json', () => {
+  // console.log('callback - particles.js config loaded');
+  if (getTheme() === 'dark') {
+    setParticleDarkTheme();
+  }
+});
+
 // dark theme toggle
 function toggleTheme() {
   if (!getTheme()) {
@@ -6,10 +15,26 @@ function toggleTheme() {
   if (getTheme() === 'dark') {
     document.documentElement.setAttribute('data-theme', 'light');
     setTheme('light');
+    setParticleLightTheme();
   } else if (getTheme() === 'light') {
     document.documentElement.setAttribute('data-theme', 'dark');
     setTheme('dark');
+    setParticleDarkTheme();
   }
+}
+
+function setParticleLightTheme() {
+  window.pJSDom[0].pJS.particles.shape.stroke.color = "#000000";
+  window.pJSDom[0].pJS.particles.color.value = "#000000";
+  window.pJSDom[0].pJS.particles.line_linked.color = "#000000";
+  window.pJSDom[0].pJS.fn.particlesRefresh();
+}
+
+function setParticleDarkTheme() {
+  window.pJSDom[0].pJS.particles.shape.stroke.color = "#fff";
+  window.pJSDom[0].pJS.particles.color.value = "#fff";
+  window.pJSDom[0].pJS.particles.line_linked.color = "#fff";
+  window.pJSDom[0].pJS.fn.particlesRefresh();
 }
 
 function getTheme() {
@@ -18,7 +43,6 @@ function getTheme() {
 function setTheme(t) {
   localStorage.setItem('theme', t);
 }
-
 
 // nav bar toggle
 let navbar = false;
@@ -35,3 +59,62 @@ function toggleNav() {
     navbar = false;
   }
 }
+
+// typer
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 150 ;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 600;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
